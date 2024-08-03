dead3edf452d5879c19e8977eddc129c8fddcafb
Saurabh Sonune <119730715+sauruvibes@users.noreply.github.com>
2024-08-03 23:55:11 +0530

    loops.cpp

--- loops.cpp
+++ loops.cpp
@@ -12,3 +12,23 @@ int main(){
     }
     return 0;
 } 
+
+//[B] Even Numbers
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    bool flag = 0;
+    for(int i=1; i<=n; i++){
+        if(i % 2 == 0){
+            cout<<i;
+            cout<<endl;
+            flag = 1;
+        }
+    }
+        if(flag == 0){
+            cout<<"-1\n";
+        }
+    return 0;
+}
