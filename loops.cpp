3950dd69c3c430467db846bb08822abd577fb8a3
Saurabh Sonune <119730715+sauruvibes@users.noreply.github.com>
2024-08-03 23:05:14 +0530

    loops.cpp

---
+++ loops.cpp
@@ -0,0 +1,14 @@
+//[ A ] 1 to N
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+ 
+    for(int i=1; i<=n; i++){
+        cout<<i;
+    
+    cout<<endl;
+    }
+    return 0;
+} 
