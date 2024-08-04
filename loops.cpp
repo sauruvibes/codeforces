c6ea6975df8fcb4a9bff32e3d43936118604aa79
Saurabh Sonune <119730715+sauruvibes@users.noreply.github.com>
2024-08-04 19:04:58 +0530

    loops.cpp

--- loops.cpp
+++ loops.cpp
@@ -35,3 +35,32 @@ int main(){
 
 
 //D. Fixed Password
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    while(true){
+        cin>>x;
+        if(x == 1999){
+            cout<<"Correct\n";
+            break;
+        }else{
+            cout<<"Wrong\n";
+        }
+    }
+    return 0;
+}
+
+//F. Multiplication table
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+
+    for(int i=1; i<=12; i++){
+        cout<<n<<" * "<<i<<" = "<<(n*i);
+        cout<<endl;
+    }
+    return 0;
+}
