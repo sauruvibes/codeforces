dfb74249a27e71d3c3108233f8f809806d0f7329
Saurabh Sonune <119730715+sauruvibes@users.noreply.github.com>
2024-08-05 16:00:22 +0530

    data_types.cpp

--- data_types.cpp
+++ data_types.cpp
@@ -11,3 +11,13 @@ int main(){
     cout<<area;
     return 0;
 }
+
+//A. Say Hello With C++
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    string name;
+    cin>>name;
+    cout<<"Hello, "<<name;
+    return 0;
+}
