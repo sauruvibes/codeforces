2a42d700162604d294020e134cc2ac9942649684
Saurabh Sonune <119730715+sauruvibes@users.noreply.github.com>
2024-08-05 15:41:23 +0530

    loops.cpp

--- loops.cpp
+++ loops.cpp
@@ -65,3 +65,18 @@ int main(){
     }
     return 0;
 }
+
+//O. Pyramid
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
