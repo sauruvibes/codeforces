e4c28859ad0e7aa941a38b5eb693939fb78fe3cd
Saurabh Sonune <119730715+sauruvibes@users.noreply.github.com>
2024-08-06 01:22:11 +0530

    data_types.cpp

--- data_types.cpp
+++ data_types.cpp
@@ -58,3 +58,19 @@ int main(){
     cout<<area;
     return 0;
 }
+
+//J. Multiples
+#include<iostream>
+using namespace std;
+int main(){
+    int a, b;
+    cin>>a>>b;
+    if(a % b == 0){
+        cout<<"Multiples\n";
+    }else if(b % a == 0){
+        cout<<"Multiples\n";
+    }else{
+        cout<<"No Multiples\n";
+    }
+    return 0;
+}
