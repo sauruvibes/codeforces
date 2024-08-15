2ffd1fc79036f23876ba93aa8be1b26db8b9eacb
Saurabh Sonune <119730715+sauruvibes@users.noreply.github.com>
2024-08-15 22:41:25 +0530

    loops.cpp

--- loops.cpp
+++ loops.cpp
@@ -96,4 +96,20 @@ int main(){
 }
 
 //T. Shape2
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    for (int i = 1; i <= n; i++) {
+        for (int j = i; j < n; j++) {
+            cout << " ";
+        }
+        for (int j = 1; j <= (2 * i - 1); j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
 
