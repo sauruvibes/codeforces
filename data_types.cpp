0e82b580654338146486de4c91adb251150c1b17
Saurabh Sonune <119730715+sauruvibes@users.noreply.github.com>
2024-08-22 19:48:02 +0530

    data_types.cpp

--- data_types.cpp
+++ data_types.cpp
@@ -197,3 +197,30 @@ int main(){
     cout<<sum<<endl;
     return 0;
 }
+
+//W. Mathematical Expression
+#include<iostream>
+using namespace std;
+int main(){
+    long long a,b,c;
+    char s,q;
+    cin>>a>>s>>b>>q>>c;
+    switch(s){
+        case '+' : if(a + b == c)
+        cout<<"Yes";
+        else
+        cout<<a+b;
+        break;
+        case '-' : if(a - b == c)
+        cout<<"Yes";
+        else
+        cout<<a-b;
+        break;
+        case '*' : if(a * b == c)
+        cout<<"Yes";
+        else
+        cout<<a*b;
+        break;        
+    }
+    return 0;
+}
