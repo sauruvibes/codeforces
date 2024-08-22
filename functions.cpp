3e6bb9f065d93776c8d42349202117054f7d46f2
Saurabh Sonune <119730715+sauruvibes@users.noreply.github.com>
2024-08-23 02:01:45 +0530

    functions.cpp

---
+++ functions.cpp
@@ -0,0 +1,12 @@
+//A. Add
+#include<iostream>
+using namespace std;
+double sum(double a, double b){
+    return a+b;
+}
+int main(){
+    double a,b;
+    cin>>a>>b;
+    cout<<sum(a,b);
+    return 0;
+}
