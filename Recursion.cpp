ae6de486aa8960459916df6f150266cb82a5cdd7
Saurabh Sonune <119730715+sauruvibes@users.noreply.github.com>
2024-08-25 19:24:35 +0530

    Recursion.cpp

---
+++ Recursion.cpp
@@ -0,0 +1,11 @@
+//A. Print Recursion
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1; i<=n; i++){
+        cout<<"I love Recursion"<<endl;
+    }
+    return 0;
+}
