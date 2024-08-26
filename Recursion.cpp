ee68c32ad521ee34c25c46c746a96573802c78d7
Saurabh Sonune <119730715+sauruvibes@users.noreply.github.com>
2024-08-26 16:15:30 +0530

    Recursion.cpp

--- Recursion.cpp
+++ Recursion.cpp
@@ -10,3 +10,15 @@ int main(){
     }
     return 0;
 }
+
+//B. Print from 1 to N
+#include<iostream>
+using namespace std;
+int main(){
+    double n;
+    cin>>n;
+    for(int i=1; i<=n; i++){
+        cout<<i<<endl;
+    }
+    return 0;
+}
