c1879190d0d30b7cc4a68dba75b7d9d49eb6ef0d
Saurabh Sonune <119730715+sauruvibes@users.noreply.github.com>
2024-08-27 18:31:53 +0530

    Strings.cpp

---
+++ Strings.cpp
@@ -0,0 +1,12 @@
+//A. Create A New String
+
+#include<iostream>
+using namespace std;
+int main(){
+    string s,t;
+    cin>>s>>t;
+    cout<<s.length()<<" "<<t.length()<<endl;
+    cin>>s>>t;
+    cout<<s<<" "<<t<<endl;
+    return 0;
+}
