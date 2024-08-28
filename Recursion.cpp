e5272cc7922cc23d0da2c2481fc5da91b2d3abff
Saurabh Sonune <119730715+sauruvibes@users.noreply.github.com>
2024-08-28 17:43:07 +0530

    Recursion.cpp

--- Recursion.cpp
+++ Recursion.cpp
@@ -21,3 +21,23 @@ int main(){
     }
     return 0;
 }
+
+
+//G. Pyramid
+#include<iostream>
+using namespace std;
+int main(){
+    double n;
+    cin>>n;
+
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n-i; j++){
+            cout<<" ";
+        }
+        for(int j=1; j<=(i*2)-1; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
