460f58df80226fb7ef5bc857a90e5f2a07c70e00
Saurabh Sonune <119730715+sauruvibes@users.noreply.github.com>
2024-10-01 01:10:19 +0530

    A_Watermelon.cpp

---
+++ A_Watermelon.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    long long x;
+    cin>>x;
+    if(x%2 == 0 && x > 2){
+        cout<<"YES\n";
+    }
+    else{
+        cout<<"NO\n";
+    }
+    return 0;
+}
